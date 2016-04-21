92d5c7f45a581b4c9d2b98a58257e1752e37b2ef
yao.ni <yao.ni@ele.me>
2016-04-22 00:56:56 +0800

    feat: string category method

--- NYDiaryOC/NYDiaryOC/Category/NSString+DiaryString.h
+++ NYDiaryOC/NYDiaryOC/Category/NSString+DiaryString.h
@@ -7,9 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NSString (DiaryString)
 
 + (NSString *)parameterStringWithParameters:(NSDictionary *)parameters;
+
+- (CGFloat)heightForScreenWithFont:(UIFont *)font;
+
+- (CGFloat)heightWithSize:(CGSize)size font:(UIFont *)font;
+
+- (CGFloat)heightWithWidth:(CGFloat)width font:(UIFont *)font;
 
 @end

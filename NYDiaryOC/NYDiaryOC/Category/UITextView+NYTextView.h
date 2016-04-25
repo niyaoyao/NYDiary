335ab4389560d1f0dc03b8e415dfacd1b37da5ef
yao.ni <yao.ni@ele.me>
2016-04-25 13:54:42 +0800

    chore: textview category

--- NYDiaryOC/NYDiaryOC/Category/UITextView+NYTextView.h
+++ NYDiaryOC/NYDiaryOC/Category/UITextView+NYTextView.h
@@ -11,7 +11,32 @@
 @interface UITextView (NYTextView)
 
 typedef NS_ENUM(NSInteger, NYTextViewActionType) {
-    NYTextViewActionType,
+    NYTextViewActionTypeShouldBeginEditing,
+    NYTextViewActionTypeShouldEndEditing,
+    NYTextViewActionTypeDidBeginEditing,
+    NYTextViewActionTypeDidEndEditing,
+    NYTextViewActionTypeShouldChangeTextInRange,
+    NYTextViewActionTypeDidChange,
+    NYTextViewActionTypeDidChangeSelection,
+    NYTextViewActionTypeShouldInteractWithURL,
+    NYTextViewActionTypeShouldInteractWithTextAttachment,
 };
+
+typedef void (^NYTextViewAction)(NYTextViewActionType actionType);
+
+/**
+ *  设置占位字符串
+ *
+ *  @param placeholderString 占位字符串
+ *  @param frame             相对于 TextView 的位置
+ */
+- (void)setupPlaceholderString:(NSString *)placeholderString frame:(CGRect)frame;
+
+/**
+ *  设置 TextView 回调方法
+ *
+ *  @param action 回调方法 @see NYTextViewAction
+ */
+- (void)setupTextViewAction:(NYTextViewAction)action;
 
 @end

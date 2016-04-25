2b94c5c24dc6451b5a328d33ede9e08eb8a2005e
yao.ni <yao.ni@ele.me>
2016-04-25 13:15:59 +0800

    chore: UITextView placeholder

---
+++ NYDiaryOC/NYDiaryOC/Category/UITextView+NYTextView.h
@@ -0,0 +1,17 @@
+//
+//  UITextView+NYTextView.h
+//  NYDiaryOC
+//
+//  Created by NiYao on 4/25/16.
+//  Copyright © 2016 niyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (NYTextView)
+
+typedef NS_ENUM(NSInteger, NYTextViewActionType) {
+    NYTextViewActionType,
+};
+
+@end
---
+++ NYDiaryOC/NYDiaryOC/Category/UITextView+NYTextView.m
@@ -0,0 +1,24 @@
+//
+//  UITextView+NYTextView.m
+//  NYDiaryOC
+//
+//  Created by NiYao on 4/25/16.
+//  Copyright © 2016 niyao. All rights reserved.
+//
+
+#import "UITextView+NYTextView.h"
+
+@interface UITableView () <UITextViewDelegate>
+
+@property (nonatomic, strong) UILabel *placeholderLabel;
+@property (nonatomic, copy)
+
+@end
+
+@implementation UITextView (NYTextView)
+
+#pragma mark - UITextViewDelegate
+
+
+
+@end

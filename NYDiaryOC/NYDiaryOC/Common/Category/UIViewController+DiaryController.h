cb5394ec37e6d71095d6d006de91b16e031cd890
yao.ni <yao.ni@ele.me>
2016-04-27 20:19:34 +0800

    feat: UI

---
+++ NYDiaryOC/NYDiaryOC/Common/Category/UIViewController+DiaryController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+DiaryController.h
+//  NYDiaryOC
+//
+//  Created by NiYao on 4/27/16.
+//  Copyright © 2016 niyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (DiaryController)
+
+@end
---
+++ NYDiaryOC/NYDiaryOC/Common/Category/UIViewController+DiaryController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+DiaryController.m
+//  NYDiaryOC
+//
+//  Created by NiYao on 4/27/16.
+//  Copyright © 2016 niyao. All rights reserved.
+//
+
+#import "UIViewController+DiaryController.h"
+
+@implementation UIViewController (DiaryController)
+
+@end
